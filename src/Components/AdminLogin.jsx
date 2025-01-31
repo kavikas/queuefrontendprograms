33602dbe8beb86dd44e6a120aa4c7c6347495f96
kavika <kavikas732006@gmail.com>
2025-01-31 09:13:57 +0530

    changed

--- src/Components/AdminLogin.jsx
+++ src/Components/AdminLogin.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./AdminLogin.css";
+import "./Adminlogin.css";
 
 const AdminLogin = () => {
   const [username, setUsername] = useState("");
